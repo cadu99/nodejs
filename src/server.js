b79695393bfcfae64ee00a5fc84579467558aa15
cadu99 <87390779+cadu99@users.noreply.github.com>
2024-09-02 20:28:41 -0300

    tree

--- src/server.js
+++ src/server.js
@@ -2,6 +2,19 @@ import http from 'node:http'
 
 
 const server = http.createServer((req, res) => {
+    const { method , url } = req
+
+    if (method === 'GET' && url === '/users'){
+        return res.end('show users')
+    }
+
+    
+    if (method === 'POST' && url === '/users'){
+        return res.end('register users')
+    }
+
+    
+
     return res.end('hello wold313')
 })
 
