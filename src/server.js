168f336b48baf19298d9704376da0a89a8632531
cadu99 <87390779+cadu99@users.noreply.github.com>
2024-09-02 20:37:52 -0300

    for

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
 import http from 'node:http'
 
+const users = []
+
 
 const server = http.createServer((req, res) => {
     const { method , url } = req
 
     if (method === 'GET' && url === '/users'){
-        return res.end('show users')
+        return res.end(JSON.stringify(users))
     }
 
     
     if (method === 'POST' && url === '/users'){
-        return res.end('register users')
+        users.push({
+            id: 1,
+            name: 'carlos',
+            email: 'arroba@gmail.com',
+        })
+        return res.end('register user')
     }
 
     
